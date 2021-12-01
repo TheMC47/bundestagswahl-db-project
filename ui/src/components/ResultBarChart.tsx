9899e1f2fcc69b9caffb7018dfddf88cffdc7b41
MaisaBS <maiisabensalah@gmail.com>
2021-12-01 15:19:04 -0500

    added first version of the barchart

--- ui/src/components/ResultBarChart.tsx
+++ ui/src/components/ResultBarChart.tsx
@@ -1,155 +1,78 @@
-import React, { useEffect, useRef } from 'react';
-import { select } from 'd3';
-import { ElectionResults } from '../App';
+import React, { useEffect, useRef, RefObject, Component } from 'react';
+import * as d3 from 'd3';
+import { ElectionResult } from '../App';
 
-/* export const useD3 = (renderChartFn, dependencies) => {
-*   const ref = React.useRef();
-*
-*   React.useEffect(() => {
-*     renderChartFn(d3.select(ref.current));
-*     return () => { };
-*   }, dependencies);
-*   return ref;
-* } */
 
-function ResultBarChart({ data }: ElectionResults): JSX.Element {
-  /* const ref = useD3(
-*   (svg) => {
-*     const height = 500;
-*     const width = 500;
-*     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-*     const x = d3
-*       .scaleBand()
-*       .domain(data.map((d) => d.candidate))
-*       .rangeRound([margin.left, width - margin.right])
-*       .padding(0.1);
+class ResultBarChart extends Component {
+  ref: RefObject<HTMLDivElement>
+  data: ElectionResult[]
+  title: string
 
-*     const y1 = d3
-*       .scaleLinear()
-*       .domain([0, d3.max(data, (d) => d.value)])
-*       .rangeRound([height - margin.bottom, margin.top]);
+  constructor( props: IResultBarChartProps ) {
+    super(props)
+    this.state = {
 
-*     const xAxis = (g) =>
-*       g.attr('transform', `translate(0,${height - margin.bottom})`).call(
-*         d3
-*           .axisBottom(x)
-*           .tickValues(
-*             d3
-*               .ticks(...d3.extent(x.domain()), width / 40)
-*               .filter((v) => x(v) !== undefined)
-*           )
-*           .tickSizeOuter(0)
-*       );
+    }
+    this.ref = React.createRef()
+    this.data =
+     [{
+      candidate: 'AFD',
+      value: 25,
+      color: '#000000'
+    },
+    {
+      candidate: 'SPD',
+      value: 30,
+      color: '#00a2ee'
+    },
+    {
+      candidate: 'Grüne',
+      value: 40,
+      color: '#fbcb39'
+    }]
+  this.title = 'Erstimmenanteil'
+  }
 
-*     const y1Axis = (g) =>
-*       g
-*         .attr('transform', `translate(${margin.left},0)`)
-*         .style('color', 'steelblue')
-*         .call(d3.axisLeft(y1).ticks(null, 's'))
-*         .call((g) => g.select('.domain').remove())
-*         .call((g) =>
-*           g
-*             .append('text')
-*             .attr('x', -margin.left)
-*             .attr('y', 10)
-*             .attr('fill', 'currentColor')
-*             .attr('text-anchor', 'start')
-*             .text(data.y1)
-*         );
 
-*     svg.select('.x-axis').call(xAxis);
-*     svg.select('.y-axis').call(y1Axis);
+  componentDidMount() {
+      this.drawChart()
+  }
 
-*     svg
-*       .select('.plot-area')
-*       .attr('fill', 'steelblue')
-*       .selectAll('.bar')
-*       .data(data)
-*       .join('rect')
-*       .attr('class', 'bar')
-*       .attr('x', (d) => x(d.candidate))
-*       .attr('width', x.bandwidth())
-*       .attr('y', (d) => y1(d.value))
-*       .attr('height', (d) => y1(0) - y1(d.value));
-*   },
-*   [data.length]
-* ); */
+  drawChart() {
+        const size = 500
+        const margin = 80;
+        const svg = d3.select(this.ref.current).append('svg').attr('width', size).attr('height', size)
+        const rectwidth = 95
 
-  const ref = useRef<SVGSVGElement | null>(null);
+        svg.selectAll('rect').data(this.data).enter().append('rect').attr('x', (d,i)=> 5 + i*(rectwidth + 5) )
+        .attr('y', (d:ElectionResult) => d.value)
+        .attr('width', rectwidth).attr('height', d => d.value)
+        .attr('fill', d => d.color)
 
-  useEffect(() => {
-    console.log(data)
+        svg.append('text')
+        .attr('class', 'title')
+        .attr('x', size / 2 + margin)
+        .attr('y', 15)
+        .attr('text-anchor', 'middle')
+        .text(this.title)
 
-    const svg = select(ref.current)
-    const height = 500;
-    const width = 500;
-    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+  }
 
-    /* const x = d3
-*   .scaleBand()
-*   .domain(data.map((d) => d.candidate))
-*   .rangeRound([margin.left, width - margin.right])
-*   .padding(0.1);
+render()  {
+    return (
+      <div className="ResultBarChart" ref= {this.ref} />
+    );
+  }
 
-* const y1 = d3
-*   .scaleLinear()
-*   .domain([0, 100])
-*   .rangeRound([height - margin.bottom, margin.top]); */
 
-    /* const xAxis = (g) =>
-* g.attr('transform', `translate(0,${height - margin.bottom})`).call(
-*   d3
-*     .axisBottom(x)
-*     .tickValues(
-*       ticks(...d3.extent(x.domain()), width / 40)
-*         .filter((v) => x(v) !== undefined)
-*     )
-*     .tickSizeOuter(0)
-* );
-*/
-    /* const y1Axis = (g) =>
-*   g
-*     .attr('transform', `translate(${margin.left},0)`)
-*     .style('color', 'steelblue')
-*     .call(d3.axisLeft(y1).ticks(null, 's'))
-*     .call((g) => g.select('.domain').remove())
-*     .call((g) =>
-*       g
-*         .append('text')
-*         .attr('x', -margin.left)
-*         .attr('y', 10)
-*         .attr('fill', 'currentColor')
-*         .attr('text-anchor', 'start')
-*         .text(data.y1)
-*     );
 
-* svg.select('.x-axis').call(xAxis);
-* svg.select('.y-axis').call(y1Axis); */
-
-    svg
-      .selectAll('.bar')
-      .data(data)
-      .join('rect')
-      /* .attr('class', 'bar') */
-      .attr('x', (d, i) => i*30)
-      .attr('width', 30)
-      .attr('y', (d, _) => d.value)
-    /* .attr('height', (d) => y1(0) - y1(d.value)); */
-  })
-
-  return (
-    <svg
-      ref={ref}
-      style={{
-        height: 500,
-        width: '100%',
-        marginRight: '0px',
-        marginLeft: '0px',
-      }}
-    >
-    </svg>
-  );
 }
+
+interface IResultBarChartProps {
+  data:ElectionResult[]
+}
+
+
 
 export default ResultBarChart;
