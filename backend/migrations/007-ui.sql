4035ed372c1efa37275a6d9267575a0cda6a0842
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-06 21:15:32 +0100

    Added deputies

--- backend/migrations/007-ui.sql
+++ backend/migrations/007-ui.sql
@@ -8,3 +8,38 @@ FROM sitze_pro_partei sp
 GRANT SELECT ON sitze_pro_partei_full TO web_anon;
 
 -- Q2
+
+CREATE FUNCTION null_or_space(TEXT) RETURNS TEXT AS
+$$
+  SELECT
+    (
+      CASE
+        WHEN $1 IS NOT NULL
+        THEN $1 || ' '
+        ELSE ''
+      END
+    )
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION full_name(kandidaten) RETURNS TEXT AS
+$$
+  SELECT null_or_space($1.titel)
+    || null_or_space($1.namenszusatz)
+    || $1.nachname
+    || ', '
+    || $1.vornamen
+$$ LANGUAGE SQL;
+
+CREATE VIEW abgeordnete(
+       name,
+       partei_kurzbezeichnung
+) AS
+SELECT
+  full_name(k),
+  p.kurzbezeichnung
+FROM bundestagsmandaten bm
+  JOIN parteikandidaturen pk ON pk.id = bm.partei
+  JOIN parteien p ON p.id = pk.partei
+  JOIN kandidaten k ON k.id = bm.kandidat;
+
+GRANT SELECT ON abgeordnete TO web_anon;
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Col, Container } from 'react-bootstrap';
 import SeatDistribution from './components/SeatDistribution';
+import Deputies from './components/Deputies'
 
 function App(): JSX.Element {
 
@@ -9,6 +10,9 @@ function App(): JSX.Element {
     <Container className="App d-flex justify-content-center">
       <Col className="mw-75">
         <SeatDistribution />
+      </Col>
+      <Col>
+        <Deputies />
       </Col>
     </Container>
   );
