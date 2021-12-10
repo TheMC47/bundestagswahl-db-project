b87f18edb2d44eb12ee3cf8ece3657ee3671045c
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-10 22:43:47 +0100

    Added basic routing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,24 +4,33 @@ import { Col, Container, Row } from 'react-bootstrap';
 import SeatDistribution from './components/SeatDistribution';
 import Deputies from './components/Deputies'
 import RegionView from './components/RegionView'
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
 
 
   return (
-    <Container className="App d-flex justify-content-center">
-      <Row>
-        <Col className="mw-75">
-          <SeatDistribution />
-        </Col>
-        <Col>
-          <Deputies />
-        </Col>
-      </Row>
-      <Row>
-        <RegionView />
-      </Row>
-    </Container>
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Bundestagswahl</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/seat-distribution">Seat Distribution</Nav.Link>
+            <Nav.Link href="/deputies">Deputies List</Nav.Link>
+            <Nav.Link href="/region">Region Results</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container className="App d-flex justify-content-center">
+        <Routes>
+          <Route path="/seat-distribution" element={<SeatDistribution />} />
+          <Route path="/deputies" element={<Deputies />} />
+          <Route path="/region" element={<RegionView />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
